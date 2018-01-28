5bd30a87cbbc338fd1b266086b4423ae9c7cf31c
rick0930 <forasp.nettesting001@gmail.com>
2018-01-28 20:21:02 +0800

    v1

---
+++ app/src/main/java/com/demo/calculator/MainActivity.java
@@ -0,0 +1,165 @@
+package com.demo.calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvExp, tvResult;
+    String expression = "0", result = "0", temp = "0";
+    int numberCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViews();
+    }
+
+    void findViews() {
+        tvExp = (TextView) findViewById(R.id.tvExp);
+        tvResult = (TextView) findViewById(R.id.tvResult);
+        tvExp.setText("");
+        tvResult.setText(result + "");
+    }
+
+    private String checkNumberPrefix(String num) {
+        if (num.matches("0[0-9]"))
+            num = num.substring(1);
+
+        return num;
+    }
+
+    private String checkPoint(String numOrExp) {
+        if (numOrExp.matches(".+(\\.){2}$"))
+            numOrExp = numOrExp.substring(0, numOrExp.length() - 1);
+
+        if (numOrExp.matches(".+[\\+\\-x/]\\."))
+            numOrExp = numOrExp.replace(".", "0.");
+
+        return numOrExp;
+    }
+
+    private String checkInteger(String numOrExp) {
+        try {
+            double d = Double.parseDouble(numOrExp);
+            if ((int) d == d)
+                return (int) d + "";
+            else
+                return numOrExp;
+        } catch (NumberFormatException e) {
+            return numOrExp;
+        }
+    }
+
+    public void onNumberClick(View view) {
+        if(!expression.contains("Infinity")&&!expression.contains("NaN")) {
+            String num = ((Button) view).getText().toString();
+            expression += num;
+            expression = checkPoint(expression);
+            temp += num;
+            temp = checkPoint(temp);
+            temp = checkNumberPrefix(temp);
+            expression = checkNumberPrefix(expression);
+            tvResult.setText(temp);
+        }
+    }
+
+    public void onOperatorClick(View view) {
+        if(!expression.contains("Infinity")&&!expression.contains("NaN")) {
+            temp = checkInteger(temp);
+            concatenate(view);
+            show();
+        }
+    }
+
+    public void onEqual(View view) {
+        if(!expression.contains("Infinity")&&!expression.contains("NaN")) {
+            calc();
+            expression = checkInteger(result + "");
+
+            if (expression.matches("[\\-]?\\d+(\\.\\d+)?")) {
+                temp = result = expression;
+                tvResult.setText(result);
+            }
+            tvExp.setText("");
+        }
+    }
+
+    void concatenate(View view) {
+        String operator = ((Button) view).getText().toString();
+        expression += operator;
+
+        if (expression.matches(".+([\\.\\+\\-x/]{2,})$")) {
+            expression = expression.substring(0, expression.length() - 2) +
+                expression.substring(expression.length() - 1);
+        }
+        calc();
+    }
+
+    String getOperator() {
+        if (expression.matches(".+[\\+\\-x/=]$")) {
+            return expression.substring(0, expression.length() - 1).split(
+                "\\d+(\\.\\d+)?")[numberCount - 1];
+        } else {
+            return expression.split("\\d+(\\.\\d+)?")[numberCount - 1];
+        }
+    }
+
+    void calc() {
+        String[] numbersArray = expression.split("[\\+\\-x/]");
+
+        if (numbersArray[0].equals(""))
+            numberCount = numbersArray.length - 1;
+        else
+            numberCount = numbersArray.length;
+
+        if (numberCount == 0)
+            return;
+
+        if (numberCount == 1) {
+            result = tvResult.getText().toString();
+            result = checkInteger(result + "");
+        } else if (numberCount >= 2) {
+            String operator = getOperator();
+            double lastNum = Double.parseDouble(temp);
+            double result = Double.parseDouble(this.result);
+            switch (operator) {
+                case "+":
+                    result += lastNum;
+                    break;
+                case "-":
+                    result -= lastNum;
+                    break;
+                case "x":
+                    result *= lastNum;
+                    break;
+                case "/":
+                    result /= lastNum;
+                    break;
+            }
+            this.result = checkInteger(result + "");
+        }
+
+        temp = "0";
+        show();
+    }
+
+    void show() {
+        expression = checkInteger(expression);
+        tvExp.setText(expression);
+        tvResult.setText(checkInteger(result + ""));
+    }
+
+    public void onClear(View view) {
+        result = "0";
+        temp = "0";
+        numberCount = 0;
+        expression = "0";
+        tvExp.setText("");
+        tvResult.setText(result);
+    }
+}
